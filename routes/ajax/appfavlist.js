91a40c44895b53f517b9adadd91201ef4bc02b46
Yinear <yinz@me.com>
2015-01-04 18:53:52 +0800

    finished fav list

--- routes/ajax/appfavlist.js
+++ routes/ajax/appfavlist.js
@@ -5,26 +5,31 @@ var hash = require('../../lib/utils').hash;
 
 exports = module.exports = function(req, res) {
     var code = req.query.c;
-    var page = (req.query.p)?req.query.p:1;
-    var number = (req.query.n)?req.query.n:12;
+    //var page = (req.query.p)?req.query.p:1;
+    //var number = (req.query.n)?req.query.n:12;
 
     if (code == null || code.length < 24) {
-        res.end("1");
+        res.end("2");
         return;
     }
 
     var id = code.substring(0, 24);
     var pwd = repairkey(code.substring(24));
-    console.log(id);
-    console.log(pwd);
+    //console.log(id);
+    //console.log(pwd);
 
     keystone.list('User').model.findOne({ _id: id }).exec(function(err, user) {
-        console.log("err:"+err);
-        console.log(hash(user.password));
+        //console.log("err:"+err);
+        //console.log(hash(user.password));
         if (user && (pwd == hash(user.password))) {
             //user.getRelated("用户收藏");
-            console.log(user);
-            return res.json(user);
+            keystone.list('UserFav').model.findOne({ '所有者': id }).populate("文章列表", '_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间').exec(function(err, userFav) {
+                if (err) return res.end("1");
+                //console.log(userFav);
+                return res.json(userFav);
+            })
+        } else {
+            res.end("2");
         }
     });
 }
