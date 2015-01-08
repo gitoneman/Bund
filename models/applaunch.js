335e96ce4c63b2ad85f11c123402d67b7566986c
Yinear <yinz@me.com>
2015-01-08 19:45:27 +0800

    add image for app launch

--- models/applaunch.js
+++ models/applaunch.js
@@ -11,6 +11,13 @@ var AppLaunch = new keystone.List('AppLaunch', {
 AppLaunch.add({
     '名称': {type: String, required: true},
     '链接': { type: Types.Url},
+    '图片': { type: Types.LocalFile, dest:'public/upload/', prefix:'/upload',
+        format: function(item, file){
+            return '<img src="/upload/'+file.filename+'" style="max-width: 200px">'
+        },
+        filename: function(item, filename) {
+            return 'a'+item._id+require('path').extname(filename);
+        }},
     '出现统计': { type: Types.Url},
     '点击统计': { type: Types.Url},
     '发布': { type: Boolean, initial: false }
