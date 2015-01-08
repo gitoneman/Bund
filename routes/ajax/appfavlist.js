979ad608c37b06f47884d1ee23a846b8592706e2
Yinear <yinz@me.com>
2015-01-08 22:04:23 +0800

    format the ajax of app fav list

--- routes/ajax/appfavlist.js
+++ routes/ajax/appfavlist.js
@@ -25,8 +25,13 @@ exports = module.exports = function(req, res) {
             //user.getRelated("用户收藏");
             keystone.list('UserFav').model.findOne({ '所有者': id }).populate("文章列表", '_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间').exec(function(err, userFav) {
                 if (err) return res.end("1");
+                if(userFav&&userFav["文章列表"]) {
+                    return res.json(userFav["文章列表"]);
+                } else {
+                    return res.json("[]");
+                }
                 //console.log(userFav);
-                return res.json(userFav);
+
             })
         } else {
             res.end("2");
