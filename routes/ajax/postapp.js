af67d7743abb26d821eb33e595b70fad70a55dfd
Yinear <yinz@me.com>
2015-01-08 17:38:47 +0800

    filter posts by variable APP

--- routes/ajax/postapp.js
+++ routes/ajax/postapp.js
@@ -5,7 +5,7 @@ exports = module.exports = function(req, res) {
     var page = (req.query.p)?req.query.p:1;
     var number = (req.query.n)?req.query.n:12;
     var category = req.query.c;
-    var q = Post.model.find().where('状态', '已发布').select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源').populate('来源', '名称').sort('-发布时间').skip((page-1)*number).limit(number);
+    var q = Post.model.find().where('APP', true).select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源').populate('来源', '名称').sort('-发布时间').skip((page-1)*number).limit(number);
     if(category&&category!='') {
         keystone.list('PostCategory').model.findOne({ '标识': category }).exec(function(err, result) {
             if(result) {
