e2cbb737c9b154df06382ddeb9f39ff023e66fd3
Yinear <yinz@me.com>
2015-01-09 11:39:05 +0800

    add source for hot posts

--- routes/ajax/hots.js
+++ routes/ajax/hots.js
@@ -7,7 +7,7 @@ exports = module.exports = function(req, res) {
     var number = (req.query.n)?req.query.n:10;
     number = (number>30)?30:number;
 
-    var q = Post.model.find().where('状态', '已发布').select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 总点击数 当日点击数 当周点击数').limit(number);
+    var q = Post.model.find().where('状态', '已发布').select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源').populate('来源', '名称').limit(number);
     if (type == 1) {
         //today hots
         var today = new Date();
