ec49e4d5dcfc285baca4db1857a64e615873c945
Yinear <yinz@me.com>
2015-02-10 13:16:52 +0800

    add app filter for post list

--- routes/ajax/postapp.js
+++ routes/ajax/postapp.js
@@ -5,8 +5,14 @@ exports = module.exports = function(req, res) {
     var page = (req.query.p)?req.query.p:1;
     var number = (req.query.n)?req.query.n:12;
     var category = req.query.c;
-    var q = Post.model.find().where('APP', true).where('锁定.开关').ne(true).where("发布时间").lt(new Date()).select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源').populate('来源', '名称').sort('-发布时间').skip((page-1)*number).limit(number);
-    var q2 = Post.model.find().where('APP', true).where('锁定.开关', true).where('锁定.页号', page).select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源 锁定').populate('来源', '名称').sort('锁定.行号').limit(number);
+    var app = (req.query.a)?req.query.a:false;
+
+    var q = Post.model.find().where('锁定.开关').ne(true).where("发布时间").lt(new Date()).select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源').populate('来源', '名称').sort('-发布时间').skip((page-1)*number).limit(number);
+    var q2 = Post.model.find().where('锁定.开关', true).where('锁定.页号', page).select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间 来源 锁定').populate('来源', '名称').sort('锁定.行号').limit(number);
+    if (!app) {
+        q.where('APP', true);
+        q2.where('APP', true);
+    }
     if(category&&category!='') {
         keystone.list('PostCategory').model.findOne({ '标识': category }).exec(function(err, result) {
             if(result) {
