ca861adb7231dafe3da0ee7ec20c4c9348b2ee0d
Yinear <yinz@me.com>
2015-04-23 15:32:52 +0800

    add search author, add db auth

--- routes/ajax/search.js
+++ routes/ajax/search.js
@@ -26,7 +26,8 @@ exports = module.exports = function(req, res) {
         res.json("[]");
     }
     //console.log("keyword" + keyword);
-    var q = Post.model.find({'标题' : new RegExp(keyword, 'i')}).where('状态', '已发布').select('_id 标题 链接 图片 图片链接 缩略图 分享数 赞数 出现统计 点击统计 发布时间').skip((page-1)*number).limit(number);
+    var kw = new RegExp(keyword, 'i');
+    var q = Post.model.find({ $or: [ {'标题' : kw}, { '作者': kw } ] }).where('状态', '已发布').select('_id 标题 链接 图片 图片链接 缩略图 分享数 赞数 出现统计 点击统计 发布时间').skip((page-1)*number).limit(number);
     q.exec(function(err, results) {
         res.json(results);
     });
--- start_db_on_zys_mac.sh
+++ start_db_on_zys_mac.sh
@@ -3,4 +3,4 @@
 mongoPath="/Users/yinz/Applications/mongodb"
 dbPath="/Users/yinz/Desktop/db"
 
-$mongoPath/bin/mongod --dbpath $dbPath --port 28933
+$mongoPath/bin/mongod --dbpath $dbPath --port 28933 --auth
