03ead00098de67a6f1923f6d4cfd3bc9651a9ef4
Yinear <yinz@me.com>
2015-05-11 17:34:22 +0800

    Add more hots data in index

--- routes/views/index.js
+++ routes/views/index.js
@@ -20,17 +20,36 @@ exports = module.exports = function(req, res) {
 //    });
 
     // Load recent posts
+    var today = new Date();
+    today.setHours(0,0,0,0);
+    view.query('hots_today',
+        Post.model.find()
+            .where('状态', '已发布')
+            .where('最近点击日', today)
+            .select('标题 链接')
+            .sort('-当日点击数')
+            .limit('10')
+    );
+
     var thisweek = new Date();
     thisweek.setHours(0,0,0,0);
     var day = thisweek.getDay();
     var diff = thisweek.getDate() - day + (day == 0 ? -6:1);
     thisweek.setDate(diff);
-    view.query('hots',
+    view.query('hots_week',
         Post.model.find()
             .where('状态', '已发布')
             .where('最近点击周', thisweek)
             .select('标题 链接')
             .sort('-当周点击数')
+            .limit('10')
+    );
+
+    view.query('hots_all',
+        Post.model.find()
+            .where('状态', '已发布')
+            .select('标题 链接')
+            .sort('-总点击数')
             .limit('10')
     );
 
