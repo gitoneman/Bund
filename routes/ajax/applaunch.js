8063c13a3cdd009da6d41e823586ad602d2d568c
Yinear <yinz@me.com>
2015-05-13 17:50:23 +0800

    Add time for app launch

--- routes/ajax/applaunch.js
+++ routes/ajax/applaunch.js
@@ -1,8 +1,8 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-
-    var q = keystone.list('AppLaunch').model.findOne().where('发布', true).select('名称 链接 图片 出现统计 点击统计 宽带链接 窄带链接 显示时长 显示底栏 标识码');
+	var now = new Date();
+    var q = keystone.list('AppLaunch').model.findOne().where("发布时间").lte(now).where("结束时间").gte(now).select('名称 链接 图片 出现统计 点击统计 宽带链接 窄带链接 显示时长 显示底栏 标识码');
     q.exec(function(err, results) {
         res.json(results);
     });
