b4aeadd8e614261beca70537168261933c7f0018
Yinear <yinz@me.com>
2015-05-14 16:54:50 +0800

    jump to given url

--- routes/other/adjump.js
+++ routes/other/adjump.js
@@ -1,3 +1,4 @@
 exports = module.exports = function(req, res) {
-	return res.redirect("http://e.cn.miaozhen.com/r/k=2006684&p=6tLaw&ro=sm&dx=0&rt=2&ns=__IP__&ni=__IESID__&v=__LOC__&vo=3ae2bb5e3&&vr=2&o=http%3A%2F%2Fwww.omegawatches.cn%2Fbaselworld-2015%2Fcn%23!constellation-pluma");
+	var url = req.params.post
+	return res.redirect(url);
 }
