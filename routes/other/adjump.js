4229203a244c53931b1e90357fdd215de8f5a26e
Yinear <yinz@me.com>
2015-06-09 13:51:07 +0800

    adjump add a param

--- routes/other/adjump.js
+++ routes/other/adjump.js
@@ -1,6 +1,7 @@
 exports = module.exports = function(req, res) {
-	if (req.query.url == "1") {
-		return res.redirect("http://www.omegawatches.cn/baselworld-2015/cn/?mz_ca=2006684&mz_sp=6tLax&mz_sb=1#!constellation-pluma");
-	}
-	return res.redirect("http://e.cn.miaozhen.com/r/k=2006684&p=6tLaw&ro=sm&dx=0&rt=2&ns=__IP__&ni=__IESID__&v=__LOC__&vo=3ae2bb5e3&&vr=2&o=http%3A%2F%2Fwww.omegawatches.cn%2Fbaselworld-2015%2Fcn%23!constellation-pluma");
+	return res.redirect(req.params.url);
+	// if (req.query.url == "1") {
+	// 	return res.redirect("http://www.omegawatches.cn/baselworld-2015/cn/?mz_ca=2006684&mz_sp=6tLax&mz_sb=1#!constellation-pluma");
+	// }
+	// return res.redirect("http://e.cn.miaozhen.com/r/k=2006684&p=6tLaw&ro=sm&dx=0&rt=2&ns=__IP__&ni=__IESID__&v=__LOC__&vo=3ae2bb5e3&&vr=2&o=http%3A%2F%2Fwww.omegawatches.cn%2Fbaselworld-2015%2Fcn%23!constellation-pluma");
 }
