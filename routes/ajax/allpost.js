150979a68229fe1134b96fbfc9e4d8ae4e6a3170
Yinear <yinz@me.com>
2015-12-28 14:11:51 +0800

    delete sort

--- routes/ajax/allpost.js
+++ routes/ajax/allpost.js
@@ -5,7 +5,7 @@ exports = module.exports = function(req, res) {
     var page = (req.query.p)?req.query.p:1;
     var number = (req.query.n)?req.query.n:12;
 
-    var q = Post.model.find().select('_id 标题 链接 缩略图 图片链接 分享数 赞数 出现统计 点击统计 发布时间').sort('-发布时间').skip((page-1)*number).limit(number);
+    var q = Post.model.find().select('_id 标题 链接 发布时间').skip((page-1)*number).limit(number);
     q.exec(function(err, results) {
         res.json(results);
     });
